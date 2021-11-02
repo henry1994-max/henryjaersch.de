467890bd2ff512028f274f1478f9f45461c64f5e
henry jaersch <henry.jaersch@gmx.net>
2021-11-02 21:51:40 +0100

    added images and started work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'henryjaersch';
+export class AppComponent implements OnInit { 
+  images = ['italy.jpg', 'sunset.jpg', 'road.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+    
+    setTimeout(() => {
+      this.showImage = true;
+      }, 100);
+    }, 8000);
+
+  }
+
+
+
 }
