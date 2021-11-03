6c4d6a6693e230685f026f11c32087c3d0e40d17
henry jaersch <henry.jaersch@gmx.net>
2021-11-03 20:23:50 +0100

    start header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,29 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit { 
-  images = ['italy.jpg', 'sunset.jpg', 'road.jpg'];
-  currentImage = 0;
-  showImage = true;
+export class AppComponent {
 
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-    
-    setTimeout(() => {
-      this.showImage = true;
-      }, 100);
-    }, 8000);
-
-  }
-
-
-
-}
+ }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['italy.jpg', 'sunset.jpg', 'road.jpg'];
+  headlines = [
+    'Bring engineering to the next level',
+    'Born to code',
+    'Graduated at TUM'
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+    
+    setTimeout(() => {
+      this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+}
