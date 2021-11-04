2dd518d23d6ad020474d610b0d989e8a2506ef10
henry jaersch <henry.jaersch@gmx.net>
2021-11-04 18:43:24 +0100

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideshowComponent,
-    MenuComponent
+    MenuComponent,
+    ImprintComponent,
+    DataProtectionComponent,
   ],
   imports: [
     BrowserModule,
